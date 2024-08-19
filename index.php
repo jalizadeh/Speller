fbc98afd12e7e399b6b2785bee0e0b0758cc0f37
jalizadeh <jalizade3d@yahoo.com>
2024-08-19 21:50:20 +0200

    Improved presentation

--- index.php
+++ index.php
@@ -7,19 +7,26 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<div class="container">
+<main>
+    <div class="container my-5">
+        <div class="bg-body-tertiary p-5 rounded">
+            <div class="col-sm-8 py-5 mx-auto">
+                <a href="http://www.infodomus.it/radio/codici/telefonico.htm">Gli Alfabeti Telefonici</a>
+                <br>
+                <a href="https://it.wikipedia.org/wiki/Alfabeto_telefonico_italiano">Alfabeto Telefonico Italiano</a>
+            
+                <form method="post" class="row">
+                    <div class="col">
+                        <input class="form-control" type="text" name="fullName" placeholder="Enter the name" required>
+                    </div>
+                    <div class="col">
+                        <button class="btn  btn-primary" type="submit">Spell</button>
+                    </div>
+                </form>
+            </div>
+        </div>
 
-    <a href="https://it.wikipedia.org/wiki/Alfabeto_telefonico_italiano">Alfabeto telefonico italiano</a>
-    <a href="http://www.infodomus.it/radio/codici/telefonico.htm">GLI ALFABETI TELEFONICI</a>
-    
-        <form method="post" class="row g-2">
-            <div class="col-auto">
-                <input class="form-control" type="text" name="fullName" placeholder="Enter the name" required>
-            </div>
-            <div class="col-auto">
-                <button class="btn  btn-primary" type="submit">Spell</button>
-            </div>
-        </form>
+
 
 
     <?php
@@ -64,30 +71,39 @@
         // Convert the name to uppercase
         $fullName = strtoupper($fullName);
         $parts = preg_split('/(?=\s)|(?<=\s)/', $fullName);
-        //print_r($newArray);
 
 
-        echo "<br>";
         echo "<div id=\"printableArea\">";
         echo "<h2>" . $fullName . "</h2>";
-        echo "<button class=\"btn btn-info\" onclick=\"printDiv('printableArea')\">Print the table</button>";
-        echo "<table class=\"table table-striped\" >";
-        echo "<tr><th>#</th><th>Letter</th><th>Spelling</th></tr>";
-        for ($i = 0; $i < strlen($fullName); $i++) {
-            $char = $fullName[$i];
-            echo "<tr>";
-            echo "<td>" . $i + 1 . "</td>";
-            echo "<td>$char</td>";
-            echo "<td>" . getPhonetic($char) . "</td>";
-            echo "</tr>";
+        echo "<div class=\"row\">";        
+        foreach ($parts as $part) {
+            if(strlen($part) > 0 && $part != " "){
+                echo "<div class=\"col\">";
+                echo "<table class=\"table table-striped\" >";
+                echo "<tr><th>" . $part. "</th><th>Letter</th><th>Spelling</th></tr>";
+                for ($i = 0; $i < strlen($part); $i++) {
+                    $char = $part[$i];
+                    echo "<tr>";
+                    echo "<td>" . ($i + 1) . "</td>";
+                    echo "<td>$char</td>";
+                    echo "<td>" . getPhonetic($char) . "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+                echo "</div>";
+            }
         }
-        echo "</table>";
+        echo "</div>";
         echo "</div>";
     }
+
     ?>
 
+    <button class="btn btn-info" onclick="printDiv('printableArea')">Print the table</button>
 
-</div>
+        </div>
+    </div>
+</main>
 
 <script>
         function printDiv(divName) {
